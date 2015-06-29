b0450a03d3fa5eaba1023ffb7006c48ee73f8567
cafreeman <cfreeman@alteryx.com>
2015-06-29 10:28:41 -0500

    trampoline solution and fix formatting on blockingEventLoop

--- functional-javascript/blockingEventLoop.js
+++ functional-javascript/blockingEventLoop.js
@@ -1,16 +1,16 @@
 function repeat(operation, num) {
   // modify this so it can be interrupted
-  if (num <= 0) return
+  if (num <= 0) return;
 
-  operation()
+  operation();
 
   if (num % 10 === 0) {
     setTimeout(function() {
-      repeat(operation, --num)
-    })
+      repeat(operation, --num);
+    });
   } else {
-    repeat(operation, --num)
+    repeat(operation, --num);
   }
 }
 
-module.exports = repeat
+module.exports = repeat;
